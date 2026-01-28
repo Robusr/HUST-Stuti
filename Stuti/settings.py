7038eb5c6aac5086d9a524dfcc6e1d0cbc156fa1
Robusr <Robusr@outlook.com>
2026-01-29 06:53:31 +0800

    coding

--- Stuti/settings.py
+++ Stuti/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'Stuti.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "localhost",
+        "PORT": 3306,
+        "USER": "root",
+        "PASSWORD": "12356",
+        "NAME": "stuti",
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
---
+++ stuti_app/__init__.py
@@ -0,0 +1,6 @@
+"""
+Robust 2026.1.29
+Django项目MySQL数据库连接文件
+"""
+import pymysql
+pymysql.install_as_MySQLdb()
