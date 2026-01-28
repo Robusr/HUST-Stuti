fe0e61ca105e8810442936d001002282d3e62df2
Robusr <Robusr@outlook.com>
2026-01-29 07:06:36 +0800

    coding

--- Stuti/settings.py
+++ Stuti/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "HOST": "localhost",
         "PORT": 3306,
         "USER": "root",
-        "PASSWORD": "12356",
+        "PASSWORD": "",
         "NAME": "stuti",
     }
 
