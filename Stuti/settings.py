60fbc6224b27c4dfeeb58f780301e3e34a021cf5
Robusr <Robusr@outlook.com>
2026-01-29 09:09:57 +0800

    coding

--- Stuti/settings.py
+++ Stuti/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+"""
+Robusr 2026.1.29
+配置相关组件
+"""
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,17 +40,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stuti_app.apps.StutiAppConfig'
+    # 正确的stuti_app应用配置（单独一行）
+    'stuti_app.apps.StutiAppConfig',
+    # corsheaders插件（单独一行，注意是纯字符串，不是配置类）
+    'corsheaders',#配置跨域
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',#配置跨域请求头
 ]
 
 ROOT_URLCONF = 'Stuti.urls'
@@ -115,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'zh-hans'
+TIME_ZONE = 'Asia/Shanghai'
+#更换为国内时间
 
 USE_I18N = True
 
@@ -128,3 +135,34 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+"""CORS组配置信息"""
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:8080',
+#     'http://127.0.0.1:8080',
+#     #如果这样写无法访问，就加上协议
+#     #（http://www.localhost:8080因为不同的corsheader版本可能有不同的需求）
+# )
+CORS_ORIGIN_ALLOW_ALL = True
+#是否允许ajax跨域请求时携带cookie，False表示不用，后面用不到cookie，所以关掉，避免cookie攻击
+#请求方法
+CORS_ALLOWS_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+#允许的请求头，不能用*，要写具体的请求头，不然Vue会跨域失败
+CORS_ALLOWS_HEADERS = (
+    "* "
+)
+
+"""媒体上传文件配置"""
+#指定文件获取的url路径
+MEDIA_URL = '/upimg/'
+
+#文件上传的保存路径
+MEDIA_ROOT = BASE_DIR / 'upimg'
