da033974be376304edbbd33c285369d7556b668f
Robusr <Robusr@outlook.com>
2026-02-02 04:47:20 +0800

    coding

--- stuti_app/trade/views.py
+++ stuti_app/trade/views.py
@@ -6,9 +6,14 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.generics import GenericAPIView
 
+from stuti_app.trade.models import PendingBooks
+from stuti_app.trade.serializers import PendingBooksSerializer
+
 
 class PendingBooksGenericAPIView(GenericAPIView):
     """基于GenericAPIView的约定交易视图方法 """
+    queryset = PendingBooks.objects.all()
+    serializer_class = PendingBooksSerializer
 
 
 
