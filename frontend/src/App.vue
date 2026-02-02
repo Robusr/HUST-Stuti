59938f8b8ea84277d88239a22c3a87dab2ccbf7b
Robusr <Robusr@outlook.com>
2026-02-03 06:52:39 +0800

    coding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,8 @@
 <template>
-  <div style="padding: 20px;">
-    <van-button type="primary" @click="showTip">点击我（主要按钮）</van-button>
-    <van-button type="success" style="margin-left: 10px;">普通成功按钮</van-button>
-  </div>
+  <van-nav-bar title="HUST Stuti" />
+
 </template>
 
 <script setup>
-import { showToast } from 'vant'
-const showTip = () => {
-  showToast('这是Vant的基础提示')
-}
+
 </script>
