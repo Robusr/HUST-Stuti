598909edcca01b796c60e237683acc41a676a7a6
Robusr <Robusr@outlook.com>
2026-02-03 08:48:20 +0800

    coding

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <van-nav-bar title="HUST Stuti" />
-  <van-grid :gutter="10">
-    <van-grid-item v-for="value in 8" :key="value" icon="photo-o" text="文字" />
-  </van-grid>
+
   <van-tabbar>
     <van-tabbar-item icon="home-o">首页</van-tabbar-item>
     <van-tabbar-item icon="search">书籍</van-tabbar-item>
